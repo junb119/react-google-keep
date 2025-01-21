8f1da6d8570b3a035ee8e45d6abaeae4d0e1c3b5
jun <emfowkd@gmail.com>
2025-01-21 17:51:33 +0900

    1-6 에러 페이지 생성하기

--- My_react-note-app/src/pages/ErrorPage/ErrorPage.tsx
+++ My_react-note-app/src/pages/ErrorPage/ErrorPage.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
-
+import { useNavigate } from "react-router-dom";
+import images from "../../assets";
+import { Container } from "./ErrorPage.styles";
+import { ButtonFill } from "../../styles/styles";
 const ErrorPage = () => {
+  const navigate = useNavigate();
   return (
-    <div>ErrorPage</div>
-  )
-}
+    <Container>
+      <div className="error__img">
+        <img src={images.errorImg} alt="pageNotFound" />
+      </div>
+      <div className="error__text">
+        <h1>404</h1>
+        <div>에러가 발견되었습니다.</div>
+        <ButtonFill onClick={() => navigate("/")}>
+          <span>메인 페이지로 돌아가기</span>
+        </ButtonFill>
+      </div>
+    </Container>
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
