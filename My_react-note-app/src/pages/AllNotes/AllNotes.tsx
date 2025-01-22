f56009dd3b3dd4b58db3c56d7dd41a5993cbe4c1
jun <emfowkd@gmail.com>
2025-01-22 16:55:09 +0900

    2-1 Note 메인 페이지 생성하기

--- My_react-note-app/src/pages/AllNotes/AllNotes.tsx
+++ My_react-note-app/src/pages/AllNotes/AllNotes.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import { useState } from "react";
+import { useAppDispatch, useAppSelector } from "../../hooks/redux";
+import { ButtonOutline, Container, EmptyMsgBox } from "../../styles/styles";
+import { Box, InputBox, TopBox } from "./AllNotes.styles";
+import { toggleFiltersModal } from "../../store/modal/modalSlice";
 
 const AllNotes = () => {
+  const dispatch = useAppDispatch();
+  const { mainNotes } = useAppSelector((state) => state.notesList);
+  const [filter, setFilter] = useState("");
+  const [searchInput, setSearchInput] = useState("");
   return (
-    <div>AllNotes</div>
-  )
-}
+    <Container>
+      {/* notes */}
+      {mainNotes.length === 0 ? (
+        <EmptyMsgBox>노트가 없습니다</EmptyMsgBox>
+      ) : (
+        <>
+          <TopBox>
+            <InputBox>
+              <input
+                type="text"
+                value={searchInput}
+                onChange={(e) => setSearchInput(e.target.value)}
+                placeholder="노트의 제목을 입력해주세요."
+              />
+            </InputBox>
+            <div className="notes__filter-btn">
+              <ButtonOutline
+                onClick={() => {
+                  dispatch(toggleFiltersModal(true));
+                }}
+                className="nav_btn"
+              >
+                <span>정렬</span>
+              </ButtonOutline>
+            </div>
+          </TopBox>
+          <Box>{/* Notes */}</Box>
+        </>
+      )}
+    </Container>
+  );
+};
 
-export default AllNotes
+export default AllNotes;
