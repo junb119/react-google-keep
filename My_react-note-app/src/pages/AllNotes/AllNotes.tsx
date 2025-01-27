3607200d4278de3dffb369ec27935091aba8ae94
jun <emfowkd@gmail.com>
2025-01-27 19:58:56 +0900

    2-11 노트 정렬 모달 생성하기

--- My_react-note-app/src/pages/AllNotes/AllNotes.tsx
+++ My_react-note-app/src/pages/AllNotes/AllNotes.tsx
@@ -4,14 +4,32 @@ import { ButtonOutline, Container, EmptyMsgBox } from "../../styles/styles";
 import { Box, InputBox, TopBox } from "./AllNotes.styles";
 import { toggleFiltersModal } from "../../store/modal/modalSlice";
 import getAllNotes from "../../utils/getAllNotes";
+import { FiltersModal } from "../../components";
 
 const AllNotes = () => {
   const dispatch = useAppDispatch();
   const { mainNotes } = useAppSelector((state) => state.notesList);
   const [filter, setFilter] = useState("");
   const [searchInput, setSearchInput] = useState("");
+  const { viewFiltersModal } = useAppSelector((state) => state.modal);
+
+  const filterHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFilter(e.target.value);
+  };
+  const clearHandler = () => {
+    setFilter("");
+  };
+
   return (
     <Container>
+      {/* filter modal */}
+      {viewFiltersModal && (
+        <FiltersModal
+          handleFilter={filterHandler}
+          handleClear={clearHandler}
+          filter={filter}
+        />
+      )}
       {/* notes */}
       {mainNotes.length === 0 ? (
         <EmptyMsgBox>노트가 없습니다</EmptyMsgBox>
