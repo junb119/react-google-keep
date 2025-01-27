bcfa37ce804222028fb6d0db2432a67acd3d3a85
jun <emfowkd@gmail.com>
2025-01-27 22:18:22 +0900

    2-14 Trash 페이지 생성하기

--- My_react-note-app/src/pages/TrashNotes/TrashNotes.tsx
+++ My_react-note-app/src/pages/TrashNotes/TrashNotes.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { useAppSelector } from "../../hooks/redux";
+import { Container, EmptyMsgBox } from "../../styles/styles";
+import { MainWrapper } from "../../components";
 
 const TrashNotes = () => {
-  return <div>TrashNotes</div>;
+  const { trashNotes } = useAppSelector((state) => state.notesList);
+  return (
+    <Container>
+      {trashNotes.length === 0 ? (
+        <EmptyMsgBox>노트가 없습니다</EmptyMsgBox>
+      ) : (
+        <MainWrapper notes={trashNotes} type="trash" />
+      )}
+    </Container>
+  );
 };
 
 export default TrashNotes;
