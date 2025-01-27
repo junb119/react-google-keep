3087a761b633c835c3cb1e28b541231a2d81cedc
jun <emfowkd@gmail.com>
2025-01-27 22:30:58 +0900

    2-15 TagNote 페이지 생성하기

--- My_react-note-app/src/pages/TagNotes/TagNotes.tsx
+++ My_react-note-app/src/pages/TagNotes/TagNotes.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { useParams } from "react-router-dom";
+import { useAppSelector } from "../../hooks/redux";
+import { Note } from "../../types/note";
+import { Container, EmptyMsgBox } from "../../styles/styles";
+import { MainWrapper } from "../../components";
 
 const TagNotes = () => {
-  return <div>TagNotes</div>;
+  const { name } = useParams() as { name: string };
+  const { mainNotes } = useAppSelector((state) => state.notesList);
+
+  const notes: Note[] = [];
+  mainNotes.forEach((note) => {
+    if (note.tags.find(({ tag }) => tag === name)) {
+      notes.push(note);
+    }
+  });
+  return (
+    <Container>
+      {notes.length === 0 ? (
+        <EmptyMsgBox>노트가 없습니다</EmptyMsgBox>
+      ) : (
+        <MainWrapper notes={notes} type={name} />
+      )}
+    </Container>
+  );
 };
 
 export default TagNotes;
